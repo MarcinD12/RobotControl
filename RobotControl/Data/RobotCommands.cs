c7246559afe128753c5fb74009be5a6962c7dbd8
MarcinD12 <dickdaring0@gmail.com>
2023-02-28 21:32:51 +0100

    initial commit with working web ui

---
+++ RobotControl/Data/RobotCommands.cs
@@ -0,0 +1,16 @@
+﻿namespace RobotControl.Data
+{
+    public static class RobotCommands
+    {
+        static HttpClient httpClient= new HttpClient();
+        public static  void  SendCommand(string command)
+        {
+             Console.WriteLine("command set to: "+command);
+            var content=new  StringContent(command);
+            httpClient.PutAsync($"https://localhost:44360/Robot/SetCommand?command={command}", content);
+
+        }
+
+
+    }
+}
