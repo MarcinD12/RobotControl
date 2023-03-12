27a70cbcacbfd1033797357d7fd1b3eb15dcdf96
MarcinD12 <dickdaring0@gmail.com>
2023-03-12 21:30:17 +0100

    halfturn feature added

--- RobotControl/Data/RobotCommands.cs
+++ RobotControl/Data/RobotCommands.cs
@@ -1,14 +1,27 @@
-﻿namespace RobotControl.Data
+﻿using System.Text.Json;
+namespace RobotControl.Data
 {
     public static class RobotCommands
     {
         static HttpClient httpClient= new HttpClient();
-        public static  void  SendCommand(string command)
+        public static  void  SetDirection(string command)
         {
+            Console.WriteLine(DateTime.Now);
              Console.WriteLine("command set to: "+command);
             var content=new  StringContent(command);
-            httpClient.PutAsync($"https://localhost:44360/Robot/SetCommand?command={command}", content);
-
+            httpClient.PutAsync($"https://localhost:44360/Robot/SetDirection?command={command}", content);
+        }
+        public static void SetTurn(string direction,int angle)
+        {
+            Dictionary<string,string> dict = new Dictionary<string,string>();
+            dict.Add("command", direction);
+            dict.Add("angle", angle.ToString());
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("command set to: " + angle);
+            var content = new StringContent(JsonSerializer.Serialize(dict));
+            //httpClient.PutAsync($"https://localhost:44360/Robot/SetTurn?command={content}", content);
+            var xd=httpClient.PutAsync($"https://localhost:44360/Robot/SetTurn?direction={direction}&angle={angle}", content).Result;
+            Console.WriteLine(xd);
         }
 
 
