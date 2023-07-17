692d6e9a1e9bf2ee69b9f78fa2a34ea0f9b30478
MarcinD12 <dickdaring0@gmail.com>
2023-07-17 19:45:05 +0200

    rearrange data format, add mode view

---
+++ RobotControl/Data/RobotCommunication.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace RobotControl.Data
+{
+    public static class RobotSteering
+
+    {
+        private static Dictionary<string, string> DataPackage = new Dictionary<string, string>() { { "mode", "idle" }, { "command", "stop" }, { "angle", "0" }, { "step", "0" } };
+
+        private static HttpClient httpClient = new HttpClient()
+        {
+
+        };
+        public static async void SendCommands()
+        {
+            HttpContent content = new StringContent(JsonSerializer.Serialize(DataPackage), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await httpClient.PutAsync("https://localhost:44360/Robot/SetCommands", content);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+        public static void SetMode(string mode)
+        {
+            DataPackage["mode"] = mode;
+            SendCommands();
+
+        }
+        public static void SetDirectionGrid(string direction, string distance)
+        {
+           // DataPackage["mode"] = "movestep";
+            DataPackage["command"] = direction;
+            DataPackage["step"] = distance;
+            SendCommands();
+        }
+
+        public static void SetMoveSmooth(string direction)
+        {
+           // DataPackage["mode"] = "movesmooth";
+            DataPackage["command"] = direction;
+            SendCommands();
+        }
+
+        public static void RadarScan()
+        {
+            DataPackage["mode"] = "radarscan";
+        }
+
+        public static void MoveAvoidObstacles()
+        {
+            DataPackage["mode"] = "avoidobstacles";
+        }
+        public static void MoveStop()
+        {
+            DataPackage["command"] = "stop";
+            SendCommands();
+        }
+
+
+        //44360
+
+    }
+}
--- RobotControl/Program.cs
+++ RobotControl/Program.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Components.Web;
 using RobotControl.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 //builder.Services.AddSingleton<RobotCommands>();
+//builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
@@ -17,6 +20,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
+    
 }
 
 app.UseHttpsRedirection();
